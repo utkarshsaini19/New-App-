b13e850db2c2f6eeea4a086c6dec2946ef57445e
UTKARSH SAINI <utkarshsaini19@gmail.com>
2023-03-18 01:44:38 +0530

    Modified again

--- src/Components/News.js
+++ src/Components/News.js
@@ -74,8 +74,8 @@ export class News extends Component {
 
     fetchMoreData = async () => {
 
-        let temp= ++this.state.page
-        // this.setState({ page: temp })
+        // let temp= ++this.state.page
+        this.setState({ page: ++this.state.page })
         // const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
         const url1 = `https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/ImageSearchAPI?q=${this.props.category}&pageNumber=${this.state.page}&pageSize=${this.props.pageSize}&autoCorrect=true&country=${this.props.country}`;
         this.setState({ loading: true })
